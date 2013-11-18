3093a0577550356f80bfff5b42600c1654abe96b
Miguel Ángel <Miguel ngel@MiguelYMarisa>
2013-11-18 23:17:31 +0100

    Ejercicio 4 concurrencia: retocado ejercicio difícil caníbales y añadidos comentarios de explicación

--- PreparacionExamen1/src/p4/Concurrencia/CanibalesDificil/Marmita.java
+++ PreparacionExamen1/src/p4/Concurrencia/CanibalesDificil/Marmita.java
@@ -11,18 +11,18 @@ public class Marmita {
 	}
 	
 	public synchronized void comer() throws InterruptedException{
-		while(racionesRestantes==0){
-			notifyAll();
+		while(racionesRestantes==0){		//Según el problema, lo primero que hace el caníbal es mirar si hay comida.
+			notifyAll();					//Si no hay comida, despierto la hebra del cocinero y espero a que termine.
 			wait();
 		}
-		racionesRestantes--;
-		notifyAll();
+		racionesRestantes--;				//Como la hebra que estaba esperando está esperando desde un método sincronizado, cuando haya un notify
+		notifyAll();						//y haya comida, será la primera en comer (ya que tenía el cerrojo bloqueado).
 	}
 	public synchronized void rellenar() throws InterruptedException{
-		while(racionesRestantes>0){
-			wait();
+		while(racionesRestantes>0){			//Mientras que la marmita tenga comida, el cocinero espera. Si llega un notify del caníbal (no hay comida)
+			wait();							//se despierta la hebra, comprueba que no hay comida y llena la marmita de nuevo.
 		}
 		racionesRestantes=numeroRaciones;
-		notifyAll();
+		notifyAll();						//Una vez se llena la marmita, notifico a la hebra que había llamado al cocinero de que ya hay comida.
 	}
 }
